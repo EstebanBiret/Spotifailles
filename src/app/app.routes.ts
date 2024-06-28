46d119a54296d881d944db4336a324c40bc6434e
Esteban BIRET-TOSCANO <100210730+EstebanBiret@users.noreply.github.com>
2024-06-28 11:10:31 +0200

    deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CallbackComponent } from './callback/callback.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, data: { standalone: true } },
+  { path: 'callback', component: CallbackComponent, data: { standalone: true } },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
